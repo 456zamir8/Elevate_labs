a15d7299e2c69b16f205c2e4f66da08eced979e0
Zamir <119868139+456zamir8@users.noreply.github.com>
2025-06-27 19:42:02 +0530

    Create Query.sql

---
+++ Task_3/Query.sql
@@ -0,0 +1,12 @@
+-- Extracting the members who had the birthday date 20 and querying the first occurence.
+
+SELECT 
+    MemberID, 
+    CONCAT(FirstName, ' ', LastName) AS FullName
+FROM 
+    Members
+WHERE 
+    DAY(DateOfBirth) = 20
+ORDER BY 
+    MemberID ASC
+LIMIT 1;
