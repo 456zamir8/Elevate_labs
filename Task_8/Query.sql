75ad5ceb7b1d496c2572d8b7a9253f4a4ff639fb
Zamir <119868139+456zamir8@users.noreply.github.com>
2025-07-15 16:36:32 +0530

    Update Query.sql

--- Task_8/Query.sql
+++ Task_8/Query.sql
@@ -1,1 +1,40 @@
+-- Show all loan by given member id
+DELIMITER $$
 
+CREATE PROCEDURE GetLoansByMember(IN member_id INT)
+BEGIN
+    SELECT 
+        l.LoanID,
+        b.Title,
+        l.LoanDate,
+        l.DueDate,
+        l.ReturnDate
+    FROM Loans l
+    JOIN Books b ON l.BookID = b.BookID
+    WHERE l.MemberID = member_id;
+END $$
+
+DELIMITER ;
+
+-- Usage
+CALL GetLoansByMember(3);
+
+-- Return how old the book is based on book ID
+DELIMITER $$
+
+CREATE FUNCTION GetBookAge(book_id INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE book_age INT;
+    SELECT YEAR(CURDATE()) - PublishedYear INTO book_age
+    FROM Books
+    WHERE BookID = book_id;
+    RETURN book_age;
+END $$
+
+DELIMITER ;
+
+-- Usage
+SELECT Title, GetBookAge(BookID) AS BookAge
+FROM Books;
