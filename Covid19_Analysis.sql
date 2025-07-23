058a766f55c00034f1640438103ca964e26e99b1
Zamir <119868139+456zamir8@users.noreply.github.com>
2025-07-24 00:54:48 +0530

    Update Covid19_Analysis.sql

--- Covid19_Analysis.sql
+++ Covid19_Analysis.sql
@@ -1,1 +1,12 @@
+-- Basic Aggregates
+-- Total Cases Globally
+SELECT 
+  SUM(Confirmed) AS TotalConfirmed,
+  SUM(Deaths) AS TotalDeaths,
+  SUM(Recovered) AS Recovered,
+  SUM(Active) AS TotalActive,
+FROM
+  covid19_data;
 
+   TotalConfirmed  TotalDeaths  TotalRecovered  TotalActive
+0     828508482.0   43384903.0     388408229.0  396715350.0
