6073c3da3128427af5d6707db07e29b627c92f52
Zamir <119868139+456zamir8@users.noreply.github.com>
2025-07-24 01:00:08 +0530

    Update Covid19_Analysis.sql

--- Covid19_Analysis.sql
+++ Covid19_Analysis.sql
@@ -1,5 +1,5 @@
 -- Basic Aggregates
--- Total Cases Globally
+-- Total Cases Globally:
 SELECT 
   SUM(Confirmed) AS TotalConfirmed,
   SUM(Deaths) AS TotalDeaths,
@@ -10,3 +10,18 @@ FROM
 
    TotalConfirmed  TotalDeaths  TotalRecovered  TotalActive
 0     828508482.0   43384903.0     388408229.0  396715350.0
+
+-- Total cases by country:
+SELECT 
+  "Country/Region" AS Country,
+  SUM(Confirmed) AS TotalConfirmed,
+  SUM(Deaths) AS TotalDeaths,
+  SUM(Recovered) AS Recovered,
+  SUM(Active) AS TotalActive,
+FROM
+  covid19_data;
+GROUP BY
+  "Country/Region"
+ORDER BY
+    TotalConfirmed DESC;
+
